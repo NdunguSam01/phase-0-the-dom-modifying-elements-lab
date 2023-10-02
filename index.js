a9a4a550737b4b5031d307c0c098078c7a28bc8d
Samuel Muigai <ndungu.muigai01@gmail.com>
2023-10-02 11:55:39 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const main=document.getElementById("main") //Getting the element with id of main
+main.remove(); //Removing the element
+
+const newHeader=document.createElement("h1") //Creating a h1 element
+newHeader.id="victory"; //Adding an id attribute to the created h1 element
+newHeader.innerHTML="Samuel Muigai is the champion"; //Setting the text of the created h1 element
+document.body.append(newHeader) //Appending the created h1 element to the document body

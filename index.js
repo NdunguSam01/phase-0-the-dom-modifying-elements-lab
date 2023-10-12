d504180374f3ea112ea177ede738f1e40034db32
Samuel Muigai <ndungu.muigai01@gmail.com>
2023-10-12 15:57:57 +0300

    Lab completed

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const main=document.getElementById("main") //Getting the element with id of main
 main.remove(); //Removing the element
 
 const newHeader=document.createElement("h1") //Creating a h1 element
-newHeader.id="victory"; //Adding an id attribute to the created h1 element
+newHeader.setAttribute("id", "victory")//Adding an id attribute to the created h1 element
 newHeader.innerHTML="Samuel Muigai is the champion"; //Setting the text of the created h1 element
 document.body.append(newHeader) //Appending the created h1 element to the document body
